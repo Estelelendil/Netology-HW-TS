d8d896d3aca07fbca0d74e3d30da76b436683ef3
Alexandra Chernova <estelelendil@mail.ru>
2022-09-05 10:11:36 +0300

    added methods to class `Card`

--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -10,4 +10,18 @@ export default class Cart {
     get items(): Buyable[] {
         return [...this._items]; 
     }
+
+    sum():number{
+      return this._items.reduce(  (summ: number, currentValue:Buyable) => summ + currentValue.price,
+      0
+    )
+    }
+    discount(discount:number):number{
+      return discount * this._items.reduce(  (summ: number, currentValue:Buyable) => summ + currentValue.price,
+      0
+    )
+    }
+    delete(id:number):Buyable[]{
+      return this._items.filter(item => item.id!==id)
+    }
 }
